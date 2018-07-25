34bd9de3873c54ac2541acc34c4610c29564ae2c
tharangni <tharangni@hotmail.com>
2018-07-25 10:10:11 +0200

    updates v6

--- no_matches.R
+++ no_matches.R
@@ -1,10 +1,10 @@
-# rm(list = ls(all=TRUE))
+rm(list = ls(all=TRUE))
 # setwd("~/Dashmote/Belgium Horeca Locations/SF Data merge R Project/")
 
 library(readxl)
 
 set.seed(123) #123, 111, 500
 
-read_file <- read_excel(path = "matched_AM_DB.xlsx", sheet = "Sheet1")
+read_file <- read_excel(path = "matched_AM_DB_temp.xlsx", sheet = "Sheet1")
 temp_m <- subset(read_file, fbid == "No Match", select = colnames(read_file))
 temp_2m <- temp_m[sample(1:nrow(temp_m), 20, replace=FALSE),]
